a8cd501ea4155b5357b29faeee10a7d2ebbda3c3
Gustavo <gustavorib.rosa@gmail.com>
2020-10-16 13:51:05 -0300

    repeticao dos testes

--- EP 1/teste.py
+++ EP 1/teste.py
@@ -4,8 +4,9 @@ a = sample(range(1),1)
 print(a)
 
 for i in range(20):
+    # qnd i for igual a 2000 adicionar uma lista de 2000 numeros 
     if i in [9] :
-        a.append(randint(i,i+10000))
+        a.append(sample(range(5),5))
     #print(i)
     else:
         a.append(randint(1,10))
--- EP 1/teste2.py
+++ EP 1/teste2.py
@@ -1,7 +1,17 @@
-a = 'g'
+# talvez esse seja o for dos testes
 
-def bal(valor):
-    if valor in ['m','n','g','d'] : print(True)
-    else: print(False)
+from random import sample, randint
+import time
 
-bal(a)
+randomlist = sample(range(2000),2000)
+print(len(randomlist))
+start_time = time.time()
+
+
+for i in range(10):
+    randomlist = randomlist + sample(range(randomlist[-1]+2000),2000)
+
+print(round(time.time() - start_time, 3) , "segundos")
+print(len(randomlist))
+
+#for i in range(5): a.append(randint(1,100))
